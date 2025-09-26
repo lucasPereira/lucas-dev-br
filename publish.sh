ad5a3526efd6cc7e7ed297025719ebd1dfcc0491
Lucas Pereira da Silva <pslucasps@gmail.com>
2025-09-25 21:31:26 -0300

    Change Linkedin handle

--- publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh poseidon -C "cd projetos/github/lucas-dev-br; ./update-content.sh"
--- update-content.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git pull origin main
-sudo cp -R * /var/www/lucas.dev.br
-sudo chown -R www-data:www-data /var/www/lucas.dev.br
